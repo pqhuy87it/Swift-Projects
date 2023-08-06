91483e88d9c661fbb5c2c0a3384c61efa6ff390a
Huy Pham Quang <pqhuy87it@gmail.com>
2023-08-06 20:21:30 +0900

    .

--- Project 3 String/StringExample/Modules/AttributedString/Cells/AttributedStringCell.swift
+++ Project 3 String/StringExample/Modules/AttributedString/Cells/AttributedStringCell.swift
@@ -14,6 +14,7 @@ class AttributedStringCell: UITableViewCell {
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+        self.selectionStyle = .none
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
